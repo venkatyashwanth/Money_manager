4ad03eb768bcc18f08058f02aca8bbbe877ea452
venkat yashwanth <mudiliyashwanth@gmail.com>
2022-07-18 04:37:28 +0000

    first commit

---
+++ src/components/MoneyManager/index.js
@@ -0,0 +1,12 @@
+const transactionTypeOptions = [
+  {
+    optionId: 'INCOME',
+    displayText: 'Income',
+  },
+  {
+    optionId: 'EXPENSES',
+    displayText: 'Expenses',
+  },
+]
+
+// Write your code here
